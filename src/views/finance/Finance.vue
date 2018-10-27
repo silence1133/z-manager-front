642ce4276e501c5b42f005735a1a87fa5addd7e5
Silence <531610808@qq.com>
2018-10-27 21:09:59 +0800

    收费管理

--- src/views/finance/Finance.vue
+++ src/views/finance/Finance.vue
@@ -1,13 +1,39 @@
 <template>
-    <section>财务管理</section>
+    <section>
+        <!--工具条-->
+        <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+            <el-form :inline="true">
+                <el-form-item>
+                    <el-input v-model="filtersKeyWord" placeholder="关键字"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="getList" icon="el-icon-search" size="medium">查询</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+        <receivables-list ref="receivablesListRef"></receivables-list>
+    </section>
 </template>
 
 <script>
+    import ReceivablesList from "@/views/finance/components/ReceivablesList";
     export default {
-        name: "Finance"
+        name: "Finance",
+        components: {ReceivablesList},
+        data(){
+            return{
+                filtersKeyWord:''
+            }
+        },
+        methods:{
+            getList:function () {
+                this.$refs.receivablesListRef.getList();
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
 
 </style>
---
+++ src/views/finance/components/ReceiveFee.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ReceiveFee"
+    }
+</script>
+
+<style scoped>
+
+</style>
