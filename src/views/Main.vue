92255b52023dcdae425a41aea451acd1b991f671
Silence <531610808@qq.com>
2018-11-18 23:05:11 +0800

    代码哟话

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,23 @@
 <template>
 	<section>
-		<h1>欢迎使用，太平洋汽车城物业管理系统</h1>
+		<h1>亲爱的{{sysUserName}}，欢迎使用太平洋汽车城物业管理系统</h1>
 	</section>
 </template>
 
 <script>
 	export default {
+        name: "Main",
+        data() {
+            return{
+                sysUserName:true
+            }
+        },
         mounted() {
+            var user = sessionStorage.getItem('user');
+            if (user) {
+                user = JSON.parse(user);
+                this.sysUserName = user.name || '';
+            }
 
         }
 	}
